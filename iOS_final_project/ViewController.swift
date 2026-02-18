8869bba8763ec21afad25280ff08e938eb703eb1
Daria Zhdanok <knemune@gmail.com>
2026-02-18 13:16:02 +0300

    Initial Commit

---
+++ iOS_final_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_final_project
+//
+//  Created by Дарья Жданок on 18.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
